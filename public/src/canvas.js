a34aeb9aa4783c5845b510aee527cb87faa65acb
anupshesh <anup.shesh@gmail.com>
2020-07-29 22:13:02 -0400

    index.html

---
+++ public/src/canvas.js
@@ -0,0 +1,115 @@
+window.onload = function()
+{
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var colorInput = document.getElementById("color");
+var socket = io.connect('http://localhost:3000/');
+
+
+//Color listener
+colorInput.addEventListener('input',()=>{
+	var color = colorInput.value;
+		ctx.strokeStyle = color;
+	});
+
+
+//Variables
+var canvasx = $(canvas).offset().left;
+var canvasy = $(canvas).offset().top;
+var last_mousex = last_mousey = 0;
+var mousex = mousey = 0;
+var mousedown = false;
+var last_touchx = last_touchy = 0;
+var touchx = touchy = 0;
+var touchstart = false
+var tooltype = 'draw';
+
+
+//Touch interactions : src http://bencentra.com/code/2014/12/05/html5-canvas-touch-events.html
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+  var touch = e.touches[0];
+  e.preventDefault();
+  var mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+  e.preventDefault();
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  e.preventDefault();
+  var mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+
+//Mousedown
+$(canvas).on('mousedown', function(e) {
+    last_mousex = mousex = parseInt(e.clientX-canvasx);
+	last_mousey = mousey = parseInt(e.clientY-canvasy);
+    mousedown = true;
+});
+
+//Mouseup
+$(canvas).on('mouseup', function(e) {
+    mousedown = false;
+});
+
+
+//Mousemove
+$(canvas).on('mousemove', function(e) {
+    mousex = parseInt(e.clientX-canvasx);
+    mousey = parseInt(e.clientY-canvasy);
+    if(mousedown) {
+        ctx.beginPath();
+        if(tooltype=='draw') {
+            ctx.globalCompositeOperation = 'source-over';
+            ctx.lineWidth = 3;
+		
+        } else {
+            ctx.globalCompositeOperation = 'destination-out';
+            ctx.lineWidth = 50;
+        }
+        ctx.moveTo(last_mousex,last_mousey);
+        ctx.lineTo(mousex,mousey);
+        ctx.lineJoin = ctx.lineCap = 'round';
+        ctx.stroke();
+
+        //Establishing communication between client and server
+        console.log('sending: ' + mousex + ',' + mousey);
+
+        var data = {
+          x: mousex,
+          y: mousey
+        }
+
+        socket.emit('mousePos',data);
+
+    }
+    last_mousex = mousex;
+    last_mousey = mousey;
+
+    //Output
+    $('#output').html('current: '+mousex+', '+mousey+'<br/>last: '+last_mousex+', '+last_mousey+'<br/>mousedown: '+mousedown);
+});
+
+
+
+
+//Use draw|erase
+use_tool = function(tool) {
+    tooltype = tool; //update
+}
+
+}
+
